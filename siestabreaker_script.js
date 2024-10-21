d5bc8d5d2b62e9efcc1c89920f4f259ad3d998ef
ILOVEKDMHS <simminjun1103@gmail.com>
2024-10-21 13:32:43 +0900

    Update siestabreaker_script.js

--- siestabreaker_script.js
+++ siestabreaker_script.js
@@ -16,8 +16,8 @@ async function startWebcam() {
 async function startFaceDetection() {
     try {
         // face-api.js 모델 로드
-      faceapi.nets.tinyFaceDetector.loadFromUri("/models");
-      faceapi.nets.faceLandmark68TinyNet.loadFromUri("/models");
+        await faceapi.nets.tinyFaceDetector.loadFromUri('https://cdn.jsdelivr.net/npm/face-api.js/weights/');
+        await faceapi.nets.faceLandmark68Net.loadFromUri('https://cdn.jsdelivr.net/npm/face-api.js/weights/');
         
         // 모델이 정상적으로 로드되면 alert 메시지 띄우기
         alert("얼굴 인식 모델이 성공적으로 로드되었습니다.");
